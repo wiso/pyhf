66a10b44079862513c1e1dbf79d97d5d2f322a73
lukasheinrich <lukas.heinrich@gmail.com>
2018-08-28 01:57:05 +0200

    add new script

---
+++ validation/run_cls_singlepoint.py
@@ -0,0 +1,64 @@
+import ROOT
+import sys
+
+infile = sys.argv[1]
+
+infile = ROOT.TFile.Open(infile)
+workspace = infile.Get("combined")
+data = workspace.data("obsData")
+
+
+
+
+sbModel = workspace.obj("ModelConfig")
+poi = sbModel.GetParametersOfInterest().first()
+
+sbModel.SetSnapshot(ROOT.RooArgSet(poi))
+
+bModel = sbModel.Clone()
+bModel.SetName("bonly")
+poi.setVal(0)
+bModel.SetSnapshot(ROOT.RooArgSet(poi))
+
+ac = ROOT.RooStats.AsymptoticCalculator(data, bModel, sbModel)
+ac.SetPrintLevel(10)
+ac.SetOneSided(True)
+ac.SetQTilde(True)
+
+
+calc = ROOT.RooStats.HypoTestInverter(ac)
+calc.SetConfidenceLevel(0.95)
+calc.UseCLs(True)
+calc.RunFixedScan(3,0,2)
+
+
+#calc = ROOT.RooStats.HypoTestInverter(ac)
+#calc.SetConfidenceLevel(0.95)
+#calc.UseCLs(True)
+#calc.RunOnePoint(1.0)
+
+result = calc.GetInterval()
+
+index = 1
+
+w = result.GetExpectedPValueDist(index)
+v = w.GetSamplingDistribution()
+
+CLs_obs = result.CLs(index)
+CLs_exp = list(v)[3:-3]
+
+import json
+print(json.dumps({'CLs_obs': CLs_obs, 'CLs_exp': CLs_exp}))
+
+#import IPython
+#IPython.embed()
+
+
+plot = ROOT.RooStats.HypoTestInverterPlot("plot","plot",result)
+c = ROOT.TCanvas()
+c.SetLogy(False)
+plot.Draw("OBS EXP CLb 2CL")
+c.Draw()
+c.SaveAs('scan.pdf')
+
+
